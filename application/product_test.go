d3ae30e0c0b4f1f58ded3fafdb04b17d31a165c0
pedrojpx <pjpxavier@gmail.com>
2024-10-18 09:34:46 -0300

    enable() and tests

---
+++ application/product_test.go
@@ -0,0 +1,23 @@
+package application_test
+
+import (
+	"testing"
+
+	"github.com/pedrojpx/hexagonal-example/application"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+
+	require.Nil(t, err)
+
+	product.Price = 0
+	err = product.Enable()
+	require.Equal(t, "the price must be greate than zero do enable product", err.Error())
+}
