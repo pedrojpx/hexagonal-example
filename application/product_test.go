19b4df075b2f7f79e4aecc25b85c045c0c074daf
pedrojpx <pjpxavier@gmail.com>
2024-10-21 10:41:52 -0300

    implemented and tested IsValid()

--- application/product_test.go
+++ application/product_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/pedrojpx/hexagonal-example/application"
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 )
 
@@ -35,4 +36,29 @@ func TestProduct_Disable(t *testing.T) {
 	product.Price = 10
 	err = product.Disable()
 	require.Equal(t, "the price must be greater than zero to disable product", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+
+	require.Nil(t, err)
+
+	product.Status = "hahaha"
+	_, err = product.IsValid()
+	require.Equal(t, "status must be either enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "price must greater than or equal to zero", err.Error())
+
 }
