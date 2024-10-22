e5432bf41fe5c24f98d8f6e4dfba071ba8dcd6ac
pedrojpx <pjpxavier@gmail.com>
2024-10-22 11:01:16 -0300

    webserver initial implementation

---
+++ adapters/web/server/server.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/pedrojpx/hexagonal-example/adapters/web/handler"
+	app "github.com/pedrojpx/hexagonal-example/application"
+)
+
+type Webserver struct {
+	Service app.ProductServiceInterface
+}
+
+func MakeNewWebserver() *Webserver {
+	return &Webserver{}
+}
+
+func (w Webserver) Serve() {
+
+	r := mux.NewRouter()
+	n := negroni.New(negroni.NewLogger())
+
+	handler.MakeProductHandlers(r, n, w.Service)
+
+	http.Handle("/", r)
+
+	server := http.Server{
+		ReadHeaderTimeout: 10 * time.Second,
+		Addr:              ":8080",
+		Handler:           http.DefaultServeMux,
+		ErrorLog:          log.New(os.Stderr, "log: ", log.Lshortfile),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
